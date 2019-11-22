f83c96ce3b0085fd667912373a749faf0c3fecf8
CodyPerakslis <CodyPerakslis@noreply.github.com>
2019-11-22 08:10:52 -0600

    multiple send

--- requester.go
+++ requester.go
@@ -4,29 +4,32 @@ import (
   "github.com/open-nebula/captain/dockercntrl"
   "github.com/open-nebula/spinner/spinresp"
   "github.com/open-nebula/comms"
+  "github.com/google/uuid"
   "log"
 )
 
 func main() {
-  dialurl := "wss://4fbf1747.ngrok.io/spin"
+  dialurl := "wss://c2a13350.ngrok.io/spin"
   socket, err := comms.EstablishSocket(dialurl)
   if err != nil {return}
   var resp spinresp.Response
   socket.Start(resp)
   reader := socket.Reader()
   writer := socket.Writer()
-  container := dockercntrl.Config{
-    Image: "busybox",
-    Cmd: []string{"echo", "hello"},
-    Tty: true,
-    Name: "james",
-    Env: []string{},
-    Port: 0,
-    Limits: &dockercntrl.Limits{
-      CPUShares: 2,
-    },
+  for i := 1; i <= 5; i++ {
+    container := dockercntrl.Config{
+      Image: "busybox",
+      Cmd: []string{"echo", "hello"},
+      Tty: true,
+      Name: uuid.New().String(),
+      Env: []string{},
+      Port: 0,
+      Limits: &dockercntrl.Limits{
+        CPUShares: 2,
+      },
+    }
+    writer <- container
   }
-  writer <- container
   for {
     select {
     case data, ok := <- reader:
