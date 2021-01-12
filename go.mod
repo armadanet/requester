0b2c67cc6c313d8887895375529f45cd1e30fa62
Zhiying Liang <liang772@umn.edu>
2021-01-12 17:20:27 -0600

    cargo

--- go.mod
+++ go.mod
@@ -5,10 +5,6 @@ go 1.14
 replace github.com/armadanet/spinner => ../spinner
 
 require (
-	github.com/armadanet/captain/dockercntrl v0.0.0-20200528144743-f3807fb05a97 // indirect
-	github.com/armadanet/comms v0.0.0-20200528090635-3089ce4375d7 // indirect
-	github.com/armadanet/spinner/spinresp v0.0.0-20200221145511-5e0b9775b00d // indirect
-	github.com/open-nebula/captain v0.0.0-20190919154739-0101b09f622b // indirect
-	github.com/open-nebula/comms v0.0.0-20191121225953-aa161e63e5de // indirect
-	github.com/open-nebula/spinner v0.0.0-20190913121111-46c49f799f21 // indirect
+	github.com/armadanet/spinner v0.0.0-00010101000000-000000000000
+	google.golang.org/grpc v1.30.0
 )
--- requester.go
+++ requester.go
@@ -17,14 +17,39 @@ func  Run(dialurl string) error {
   client := spincomm.NewSpinnerClient(conn)
 
   ctx := context.Background()
-  
+
+  taskSpec := spincomm.TaskSpec{
+    Filters:     []string{"Resource"},
+    Sort:        "LeastUsage",
+    ResourceMap: map[string]*spincomm.ResourceRequirement{},
+    Ports:       map[string]string{},
+    IsPublic:    false,
+    NumReplicas: 1,
+    CargoSpec: &spincomm.CargoReq{
+      Size: 100,
+      NReplica: 3,
+    },
+  }
+  taskSpec.ResourceMap["CPU"] = &spincomm.ResourceRequirement{
+    Weight: 0.5,
+    Requested: 1,
+    Required: true,
+  }
+  taskSpec.ResourceMap["Memory"] = &spincomm.ResourceRequirement{
+    Weight: 0.5,
+    Requested: 1086522880,
+    Required: true,
+  }
+  taskSpec.Ports["8080"] = ""
 
   request := &spincomm.TaskRequest{
-    TaskId: &spincomm.UUID{Value: "test_request"},
-    Image: "docker.io/codyperakslis/armada-cargo-test",
-    Command: []string{"echo", "hello"},
+    AppId: &spincomm.UUID{Value: "test_request"},
+    Image: "goface-new",
+    Command: []string{},
     Tty: true,
     Limits: &spincomm.TaskLimits{CpuShares: 2},
+    Taskspec: &taskSpec,
+    Port: 8080,
   }
   stream, err := client.Request(ctx, request)
   if err != nil {return err}
