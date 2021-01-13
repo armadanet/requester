57948888453a6ff0e8b005eb2a5996b49158b125
Zhiying Liang <liang772@umn.edu>
2021-01-13 16:24:47 -0600

    testing

--- requester.go
+++ requester.go
@@ -19,32 +19,32 @@ func  Run(dialurl string) error {
   ctx := context.Background()
 
   taskSpec := spincomm.TaskSpec{
-    Filters:     []string{"Resource"},
-    Sort:        "LeastUsage",
+    Filters:     []string{"Resource", "Affinity"},
+    Sort:        "Geolocation",
     ResourceMap: map[string]*spincomm.ResourceRequirement{},
     Ports:       map[string]string{},
     IsPublic:    false,
     NumReplicas: 1,
     CargoSpec: &spincomm.CargoReq{
-      Size: 100,
+      Size: 1,
       NReplica: 3,
     },
   }
   taskSpec.ResourceMap["CPU"] = &spincomm.ResourceRequirement{
     Weight: 0.5,
-    Requested: 1,
+    Requested: 0,
     Required: true,
   }
   taskSpec.ResourceMap["Memory"] = &spincomm.ResourceRequirement{
     Weight: 0.5,
-    Requested: 1086522880,
+    Requested: 1,
     Required: true,
   }
   taskSpec.Ports["8080"] = ""
 
   request := &spincomm.TaskRequest{
     AppId: &spincomm.UUID{Value: "test_request"},
-    Image: "goface-new",
+    Image: "zhiying12/goface-new",
     Command: []string{},
     Tty: true,
     Limits: &spincomm.TaskLimits{CpuShares: 2},
@@ -52,6 +52,7 @@ func  Run(dialurl string) error {
     Port: 8080,
     TaskId: &spincomm.UUID{Value: "test_request_1"},
   }
+  log.Println("sending task")
   stream, err := client.Request(ctx, request)
   if err != nil {return err}
 
